b71b0be80a8a631fdcd97b8f8a5547f0145b2e99
Term <sashko@gmail.com>
2024-03-14 17:06:40 +0500

    /

--- data/news.py
+++ data/news.py
@@ -15,4 +15,3 @@ class News(SqlAlchemyBase, SerializerMixin):
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
-    is_published = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
